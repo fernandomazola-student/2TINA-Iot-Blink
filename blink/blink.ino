e8a36e5209d21a4d7eada46b1d2e502a026f1e92
Fernando Mazola <fernando_mazola@hotmail.com>
2017-08-09 11:34:45 -0300

    Logica fade led

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,17 +1,29 @@
-const int LED = 8;
+//const int LED = 8;
+const int LEDD = 9;
 
 void setup() {
 //Vamos setar o tipo de porta, se ele vai entrar ou vai sair dados. 
 //Para fazer o led piscar, precisamos fazer a saida
 
 //Avisamos ao arduino que a porta do tipo 8 será de saida, ou seja, estamos enviando energia.
-pinMode(LED, OUTPUT);
+//pinMode(LED, OUTPUT);
+pinMode(LEDD, OUTPUT);
 }
 
 void loop() {
   // Quando ligamos um circuito , ele está no loop infinito. Porque ele vai ficar fazendo a logica dele, ou seja, ta ligada, ou desligado? Vamos colocar a logica do que o arduino deve fazer
-  digitalWrite(LED, LOW);
-  delay(1000);
-  digitalWrite(LED, HIGH);
-  delay(1000);
+//  digitalWrite(LED, LOW);
+//  delay(1000);
+//  digitalWrite(LED, HIGH);
+//  delay(1000);
+
+  for(int i = 0; i <= 255; i++){
+    analogWrite(LEDD, i);
+    delay(20);
+    }
+
+    for(int i = 255; i >= 0; i--){
+    analogWrite(LEDD, i);
+    delay(20);
+    }
 }
